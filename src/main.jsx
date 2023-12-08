fc88a30e67e88ec6fd21dc6287357ca9f5590378
GerVaf <thantzinmin667@gmail.com>
2023-12-08 21:07:39 +0630

    v0

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Global/store.js";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("fb-root")).render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
