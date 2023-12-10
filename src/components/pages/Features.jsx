820f99e2eef61fe3bdbd9fe83f190fe545ad4aa1
GerVaf <113784457+GerVaf@users.noreply.github.com>
2023-12-10 19:58:46 +0630

    Update Features.jsx

--- src/components/pages/Features.jsx
+++ src/components/pages/Features.jsx
@@ -6,14 +6,18 @@ const Features = () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
   return (
     <div className="h-[100vh] overflow-scroll">
-      <div className="lg:text-4xl text-2xl font-bold text-[#2A9EB8] my-5 px-2">
-        <h1 className="pb-2 text-center">
+
+{/* title  */}
+          <div className="text-3xl font-bold text-[#2A9EB8] flex items-center text-center flex-col">
+            <h1 className="pb-2 text-center">
           ANY GAMES ANY WHERE
           <br />
           THE WORLD IS IN YOUR HANDS
         </h1>
-        <hr className="h-1 w-2/4 bg-[#2A9EB8]" />
-      </div>
+            <span className="h-1 w-40 bg-[#2A9EB8]"></span>
+          </div>      
+        
+ 
       <div className="w-full flex justify-center items-center ">
         <Cards />
       </div>
